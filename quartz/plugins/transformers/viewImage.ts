8999a23af8e18a75675bfed8bd48b069ef045e9a
chengyongru <2755839590@qq.com>
2025-09-29 15:27:33 +0800

    clean format issue

--- quartz/plugins/transformers/viewImage.ts
+++ quartz/plugins/transformers/viewImage.ts
@@ -18,7 +18,7 @@ export const ViewImage: QuartzTransformerPlugin = () => {
               }
             `,
             inline: true,
-          }
+          },
         ],
         js: [
           {
