3810eda4cb70902a7626f0cb12d9e34669b5cfb9
Harv <harveyives@users.noreply.github.com>
2025-10-18 15:38:27 +0000

    make unlistedPatterns nonoptional

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -63,7 +63,7 @@ export interface GlobalConfiguration {
   /** Glob patterns to not search */
   ignorePatterns: string[]
   /** Glob patterns to mark files as unlisted (hidden from listings but still accessible via direct link) */
-  unlistedPatterns?: string[]
+  unlistedPatterns: string[]
   /** Whether to use created, modified, or published as the default type of date */
   defaultDateType: ValidDateType
   /** Base URL to use for CNAME files, sitemaps, and RSS feeds that require an absolute URL.
