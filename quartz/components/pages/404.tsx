ddd51a6603201c8927fb5c2a933c647f3df93bbe
vcdvcd <139280960+vcdvcd214@users.noreply.github.com>
2026-01-14 02:24:01 +0800

    Localize 404 error page to Chinese
    Updated the 404 error page with localized content in Chinese.

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -8,9 +8,17 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
 
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+      <h1>此處無語</h1>
+      <p style={{ fontSize: "1.2rem", marginBottom: "1rem" }}>🪵 <b>走偏了嗎？</b></p>
+      <p>
+        可能火候未到，或是連結的路徑在修坯時不小心削去了。<br />
+        在「靜觀微語」的世界裡，有些角落還在等待泥土成型。
+      </p>
+      <p style={{ fontStyle: "italic", opacity: 0.8, marginTop: "2rem" }}>
+        「有時候，沒捏好的坯，也是一種必經的安靜。」
+      </p>
+      <br />
+      <a href={baseDir}>回到首頁靜觀</a>
     </article>
   )
 }
