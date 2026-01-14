a865828a941b393e6b56611c2727f9ebc085d9f7
vcdvcd <139280960+vcdvcd214@users.noreply.github.com>
2026-01-15 00:41:50 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,7 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/components/PageLayout"
 import * as Component from "./quartz/components"
 
-// 排序邏輯
 const customSortFn = (a, b) => {
   const nameOrder = ["about", "書籍紀錄", "隨筆紀錄", "tags"]
   const i = nameOrder.indexOf(a.name)
@@ -12,11 +11,10 @@ const customSortFn = (a, b) => {
   return (a.displayName ?? a.name).localeCompare(b.displayName ?? b.name, "zh-Hant")
 }
 
-// 修復重點：sharedPageComponents 必須包含 beforeBody: []
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  beforeBody: [], // ← 就是這裡！必須是陣列，不能漏掉
+  beforeBody: [], 
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -32,10 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer({
-      title: "🏺 全站導覽",
-      sortFn: customSortFn,
-    }),
+    Component.Explorer({ title: "🏺 全站導覽", sortFn: customSortFn }),
   ],
   right: [
     Component.Graph(),
@@ -54,10 +49,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer({
-      title: "🏺 全站導覽",
-      sortFn: customSortFn,
-    }),
+    Component.Explorer({ title: "🏺 全站導覽", sortFn: customSortFn }),
   ],
   right: [],
   center: [
