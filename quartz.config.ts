bc60c73a9b078dc27516865c91647fc38865b411
vcdvcd <139280960+vcdvcd214@users.noreply.github.com>
2026-01-15 04:07:48 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "林永章 | Lin Yung-Chang | Nick",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
   provider: "umami",
