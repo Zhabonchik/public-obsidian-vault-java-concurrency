65eca6ca524d4f54746401097b9f72880b72458d
saberzero1 <github@emilebangma.com>
2026-02-25 19:21:44 +0100

    chore: linting

--- quartz/bootstrap-cli.mjs
+++ quartz/bootstrap-cli.mjs
@@ -41,7 +41,7 @@ async function launchTui() {
   if (!existsSync(tuiPath)) {
     console.error(
       "TUI plugin not installed. Install with:\n" +
-        "  npx quartz plugin add github:quartz-community/tui\n"
+        "  npx quartz plugin add github:quartz-community/tui\n",
     )
     process.exit(1)
   }
